5fe1fcb0e9ab368174d0b9226ff9a79f167d8265
Emad Aldomari <ea@ea.sa>
2021-10-11 11:33:47 +0300

    fix cube

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const squareOrCube = (type, num) => {
     if(!["square","cube"].includes(type)) return Error("wrong method");
     if(typeof num !== "number") return Error("second parameter should be a number");
     // method
-    return Math.pow(num, type === "square" ? 2 : 4);
+    return Math.pow(num, type === "square" ? 2 : 3);
 }
 console.log( squareOrCube("square",5) );
